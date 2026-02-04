3c03f12de10801e780a7cda2d186c40f518bcb42
Agab-dev <141479415+Agab-dev@users.noreply.github.com>
2026-02-04 12:42:06 +0300

    feat(pages): created the pages folder and added the router views for the root routes and the nested ones
    Co-authored-by: Agab <gameon.ahamd@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 
 <template>
   <h1>Coach Finder</h1>
+  <router-view></router-view>
 </template>
 
 <style scoped></style>
---
+++ src/pages/coaches/CoachRegistrationForm.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Coach Registration Form</h1>
+</template>
