bcf88da57db18448da57cf272bfaf4a64b53621f
Agab <gameon.ahamd@gmail.com>
2026-02-04 15:08:38 +0300

    feat(coaches): added the dummy data for coaches, plus the template for it

---
+++ src/components/coaches/CoachItem.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { computed } from "vue";
+
+const { firstName, lastName, rate, areas } = defineProps({
+  id: {
+    required: true,
+    type: String,
+  },
+  firstName: {
+    required: true,
+    type: String,
+  },
+  lastName: {
+    required: true,
+    type: String,
+  },
+  rate: {
+    required: true,
+    type: Number,
+  },
+  areas: {
+    required: true,
+    type: Array,
+  },
+});
+
+const fullName = computed(() => `${firstName} ${lastName}`);
+</script>
+
+<template>
+  <li>
+    <h3>{{ fullName }}</h3>
+    <h4>${{ rate }}/hour</h4>
+    <div>
+      <span v-for="area in areas" :key="area">{{ area }}</span>
+    </div>
+    <div class="actions">
+      <router-link :to="{ name: 'coachContactForm', params: { id } }"
+        >Contact</router-link
+      >
+      <router-link :to="{ name: 'coachDetails', params: { id } }"
+        >View Details</router-link
+      >
+    </div>
+  </li>
+</template>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #424242;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+}
+
+h3,
+h4 {
+  margin: 0.5rem 0;
+}
+
+div {
+  margin: 0.5rem 0;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/pages/coaches/CoachList.vue
+++ src/pages/coaches/CoachList.vue
@@ -1,3 +1,45 @@
+<script setup>
+import CoachItem from "@/components/coaches/CoachItem.vue";
+import { useCoachesStore } from "@/stores/coaches";
+import { storeToRefs } from "pinia";
+import { RouterLink } from "vue-router";
+
+const { coaches, hasCoaches } = storeToRefs(useCoachesStore());
+</script>
+
 <template>
-  <h1>Coach List</h1>
+  <section>FILTER</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link :to="{ name: 'coachRegistrationForm' }"
+        >Register as Coach</router-link
+      >
+    </div>
+    <ul v-if="hasCoaches">
+      <coach-item
+        v-for="coach in coaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      ></coach-item>
+    </ul>
+    <h3 v-else>No Coaches Found</h3>
+  </section>
 </template>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
