ac5688a121d9105f00aedaf1a4f72762871426b4
Agab-dev <141479415+Agab-dev@users.noreply.github.com>
2026-02-05 13:13:00 +0300

    feat(requests): added the request list page (#11)
    Co-authored-by: Agab <gameon.ahamd@gmail.com>

---
+++ src/components/requests/RequestItem.vue
@@ -0,0 +1,44 @@
+<script setup>
+const {email, message} = defineProps({
+  email: {
+    type: String, 
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <li>
+    <div>
+      <a :href="`mailto:${email}`">{{email}}</a>
+    </div>
+    <p>{{message}}</p>
+  </li>
+</template>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #ccc;
+  padding: 1rem;
+}
+
+a {
+  color: #3d008d;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+a:hover,
+a:active {
+  color: #8d007a;
+}
+
+p {
+  margin: 0.5rem 0 0 0;
+}
+</style>
--- src/pages/requests/RequestList.vue
+++ src/pages/requests/RequestList.vue
@@ -1,3 +1,41 @@
+<script setup>
+import { useRequestsStore } from "@/stores/requests";
+import RequestItem from "@/components/requests/RequestItem.vue";
+const requestStore = useRequestsStore();
+</script>
+
 <template>
-  <h1>Request List</h1>
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests Received</h2>
+      </header>
+      <ul v-if="requestStore.hasRequests">
+        <request-item
+          v-for="request in requestStore.receivedRequests"
+          :key="request.id"
+          :message="request.message"
+          :email="request.userEmail"
+        ></request-item>
+      </ul>
+      <h3 v-else>You haven't received any requests yet!</h3>
+    </base-card>
+  </section>
 </template>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
