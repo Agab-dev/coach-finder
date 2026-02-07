f1157d2fc22bb0f0535acd05ef4bfc0b882e3fa4
Agab <gameon.ahamd@gmail.com>
2026-02-07 18:54:24 +0300

    style(routes): added animations for route transitions

--- src/pages/coaches/CoachDetails.vue
+++ src/pages/coaches/CoachDetails.vue
@@ -29,35 +29,37 @@ const areas = computed(() => selectedCoach.areas);
 </script>
 
 <template>
-  <section>
-    <base-card>
-      <h3>{{ fullName }}</h3>
-      <h4>${{ rate }}/hour</h4>
-    </base-card>
-  </section>
-  <section>
-    <base-card>
-      <header>
-        <h2>Interested? Reach Out Now!</h2>
-        <base-button
-          v-if="!contactFormIsOpen"
-          isLink
-          :to="{ name: 'coachContactForm', params: { id } }"
-          >Contact
-        </base-button>
-        <router-view></router-view>
-      </header>
-    </base-card>
-  </section>
-  <section>
-    <base-card>
-      <base-badge
-        v-for="area in areas"
-        :key="area"
-        :type="area"
-        :title="area"
-      ></base-badge>
-      <p>{{ description }}</p>
-    </base-card>
-  </section>
+  <div>
+    <section>
+      <base-card>
+        <h3>{{ fullName }}</h3>
+        <h4>${{ rate }}/hour</h4>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <header>
+          <h2>Interested? Reach Out Now!</h2>
+          <base-button
+            v-if="!contactFormIsOpen"
+            isLink
+            :to="{ name: 'coachContactForm', params: { id } }"
+            >Contact
+          </base-button>
+          <router-view></router-view>
+        </header>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <base-badge
+          v-for="area in areas"
+          :key="area"
+          :type="area"
+          :title="area"
+        ></base-badge>
+        <p>{{ description }}</p>
+      </base-card>
+    </section>
+  </div>
 </template>
